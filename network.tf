5785b6b615c62694108962331c0be43cc35aa2da
root <sjhye@megazone.com>
2023-12-28 04:38:16 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,6 +2,5 @@ module "network" {
   source  = "app.terraform.io/sjh/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "sjh"
-  use_for_each = false
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
